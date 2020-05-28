b2460db8a25bf0003c3a086b690030656592e1d1
MgBacon <mgcbkmn@googlemail.com>
2020-05-29 01:48:37 +0200

    Remove dead code
    Removed the extra checks of my code for following reasons:
    - deleting the message it responds to really messes with the flow
    - if hanamaru doesn't have permission to embed links, then the URL won't embed either so there's no point. include perms in the invite URL and/or display message to ask to enable the `EmbedLinks` permission

--- src/main/kotlin/Hanamaru.kt
+++ src/main/kotlin/Hanamaru.kt
@@ -1,11 +1,8 @@
-import com.gitlab.kordlib.common.entity.Permission
 import com.gitlab.kordlib.core.Kord
-import com.gitlab.kordlib.core.behavior.channel.createMessage
-import com.gitlab.kordlib.core.entity.channel.GuildMessageChannel
+import com.gitlab.kordlib.core.behavior.channel.createEmbed
 import com.gitlab.kordlib.core.event.gateway.ReadyEvent
 import com.gitlab.kordlib.core.event.message.MessageCreateEvent
 import com.gitlab.kordlib.core.on
-import com.gitlab.kordlib.rest.builder.message.EmbedBuilder
 import java.io.File
 import java.util.*
 
@@ -24,29 +21,11 @@ suspend fun main() {
             val url="https://cdn.discordapp.com/emojis/$emoteid.png?v=1"
             val username=message.author?.tag ?: "Invalid User"
             val channel = message.getChannel()
-            val ownembed= EmbedBuilder()
-            ownembed.apply {
+            channel.createEmbed {
                 image=url
                 footer  {
                     icon=message.author?.avatar?.url ?: message.author?.avatar?.defaultUrl
                     text=username
-                }
-            }
-            if (channel !is GuildMessageChannel){ //if not guild, then DM
-                channel.createMessage { embed=ownembed}
-            }else{
-                val permissions = channel.getEffectivePermissions(kord.selfId)
-                //delete original message if you got the perm
-                /*
-                if(Permission.ManageMessages in permissions){
-                    message.delete()
-                }
-                */
-                //check if I can actually post an embed
-                if(Permission.EmbedLinks in permissions){
-                    channel.createMessage { embed=ownembed }
-                }else{ //if not just post the URL
-                    channel.createMessage("$url by $username")
                 }
             }
         }
