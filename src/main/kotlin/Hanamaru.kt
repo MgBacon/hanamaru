b15e7402c5f91e29d40aca01bdd54d0e1ca42096
MgBacon <mgcbkmn@googlemail.com>
2020-06-06 22:14:24 +0200

    Added support for animated stickers
    Added extra regex to detect animated stickers

--- src/main/kotlin/Hanamaru.kt
+++ src/main/kotlin/Hanamaru.kt
@@ -19,33 +19,39 @@ suspend fun main() {
     }
     client.on<MessageCreateEvent> {
         val content=message.content
-        //IT'S NOT LIKE I WANT TO MATCH YOU OR ANYTHING BAKA
-        val pattern=Regex("<:[^><]+:(\\d+)>")
-        if(pattern.matches(content)) {
-            val (emoteid) = pattern.find(content)!!.destructured
-            val url="https://cdn.discordapp.com/emojis/$emoteid.png?v=1"
-            val username=message.author?.tag ?: "Invalid User"
-            val channel = message.getChannel()
-            channel.createEmbed {
-                image=url
-                footer  {
-                    icon=message.author?.avatar?.url ?: message.author?.avatar?.defaultUrl
-                    text=username
-                }
+        println(content)
+        //if null, it's not a singular emote message so we just abort
+        val url=searchMessageForEmote(content)?: return@on
+        message.getChannel().createEmbed {
+            image=url
+            footer  {
+                icon=message.author?.avatar?.url ?: message.author?.avatar?.defaultUrl
+                text=message.author?.tag ?: "Invalid User"
             }
         }
     }
     client.login()
 }
-
-fun loadToken(): String {
-    val properties: Properties = Properties()
-    properties.load(File("config.properties").inputStream())
-    return properties.getProperty("TOKEN")
-}
 fun loadProperties(): Properties {
         val properties: Properties = Properties()
         properties.load(File("config.properties").inputStream())
         return properties
+}
+
+//maybe find better name
+//checks if message fits either an animated message or regular emote syntax
+fun searchMessageForEmote(content: String): String? {
+    //IT'S NOT LIKE I WANT TO MATCH YOU OR ANYTHING BAKA
+    val patternEmote=Regex("<:[^><]+:(\\d+)>")
+    if(patternEmote.matches(content)){
+        val (emoteid) = patternEmote.find(content)!!.destructured
+        return "https://cdn.discordapp.com/emojis/$emoteid.png?v=1"
+    }
+    val patternGif=Regex("<a:[^><]+:(\\d+)>")
+    if(patternGif.matches(content)){
+        val (emoteid) = patternGif.find(content)!!.destructured
+        return "https://cdn.discordapp.com/emojis/$emoteid.gif?v=1"
+    }
+    return null
 }
 
